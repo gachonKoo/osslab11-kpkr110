8a5c92da42a148c8e99dbe9bd52c9fec8b1b188a
kpkr110 <kpkr110@naver.com>
2024-11-17 22:45:30 +0900

    Update utils.py

--- geo/utils.py
+++ geo/utils.py
@@ -7,12 +7,6 @@ def subtract(a, b):
     return a - b
 
 def hypotenuse(a, b):
-    return (a ** 2 + b ** 2) ** 0.5
-
-def circle_area(radius):
-    return 3.14 * radius ** 2
-  
-def hypotenuse(a, b):
     return math.sqrt(a**2 + b**2)
 
 def circle_area(radius):
